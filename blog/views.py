2ee9bfa2f1c1b52b8078acd086e96629ffc10708
Rolf <vdt.rolf@xs4all.nl>
2024-11-30 03:03:34 +0100

    chapter one

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import Http404
+from .models import Post
+def post_list(request):
+    posts = Post.published.all()
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+    )
+    
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found.")
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post}
+    )    
