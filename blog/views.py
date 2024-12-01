eb70877e191872311c77a18d4475ffa1cf9b7dca
Rolf <vdt.rolf@xs4all.nl>
2024-12-01 20:12:35 +0100

    pagination

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,36 @@
-from django.shortcuts import render
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from .models import Post
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer get the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(
         request,
         'blog/post/list.html',
         {'posts': posts}
     )
     
-def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
     return render(
         request,
         'blog/post/detail.html',
         {'post': post}
-    )    
+    )
