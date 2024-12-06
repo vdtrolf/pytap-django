d5c24f305727aba2111cc7fcf9c800ac1d9f25a0
vdtrolf <vdt.rolf@xs4all.nl>
2024-12-06 06:29:01 +0100

    from

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
+from django.views.generic import ListView
+
 from .models import Post
 def post_list(request):
     post_list = Post.published.all()
@@ -34,3 +36,37 @@ def post_detail(request, year, month, day, post):
         'blog/post/detail.html',
         {'post': post}
     )
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+    
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm()
+        return render(
+            request,
+            'blog/post/share.html',
+            {
+                'post': post,
+                'form': form
+            }
+        )
