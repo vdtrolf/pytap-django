e7ebbca433985aef69f5e2bb4840f55885ac6483
vdtrolf <vdt.rolf@xs4all.nl>
2024-12-12 10:37:43 +0100

    more classes

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 from .models import Post
 from .forms import EmailPostForm
 
@@ -90,5 +91,32 @@ def post_share(request, post_id):
             'post': post,
             'form': form,
             'sent': sent
+        }
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+        )
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
         }
     )
